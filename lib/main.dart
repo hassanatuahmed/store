2a3155a8e2ec0ee34e6d97c870df341c1c340a36
bwilsonabeiku <bwilsonabeiku4@gmail.com>
2021-11-11 11:06:43 +0000

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_complete_guide/screens/product_detail_screen.dart';
+import 'package:flutter_complete_guide/screens/product_overview_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.purple,
+        accentColor: Colors.deepOrange,
+        fontFamily: 'LAto',
+      ),
+      home: ProductsOverviewScreen(),
+      routes: {
+        ProductDetailScreen.routeName :(context)=>ProductDetailScreen(),
+      },
+    );
+  }
+}
+
+
