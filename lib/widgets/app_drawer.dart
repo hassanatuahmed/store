b67bccff77476a888795ed0f5f89beb54f6d5bc9
hassanatuahmed <hassanatuahmed05@gmail.com>
2021-12-16 13:23:08 +0000

    update

--- lib/widgets/app_drawer.dart
+++ lib/widgets/app_drawer.dart
@@ -13,7 +13,7 @@ class AppDrawer extends StatelessWidget {
       child: Column(
         children: <Widget>[
           AppBar(
-            title: Text('Hello Friend!'),
+            title: Text('Menu'),
             automaticallyImplyLeading: false,
           ),
           Divider(),
